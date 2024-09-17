6cb321a71e5d155232c89e356dad073ca04922d6
thinhnee <lht1823k@gmail.com>
2024-09-17 18:03:59 +0700

    Some updates
    Player animation, physics, movement. Enemy AI

---
+++ Assets/MyGame/Scripts/Enemy/EnemyAI.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AddComponentMenu("ThinhLe/EnemyAI")]
+
+
+public class EnemyAI : MonoBehaviour
+{
+
+    private enum State
+    {
+        Roaming
+    }
+    private State state;
+    private EnemyPathFinding enemyPathfinding;
+    private SpriteRenderer mySpriteRenderer;
+    private Vector2 lastPosition;
+
+    private void Awake()
+    {
+        enemyPathfinding = GetComponent<EnemyPathFinding>();
+        mySpriteRenderer = GetComponent<SpriteRenderer>();
+        state = State.Roaming;
+    }
+
+    private void Start()
+    {
+        StartCoroutine(RoamingRoutine());
+    }
+    private void Update()
+    {
+        Flip();
+    }
+    private IEnumerator RoamingRoutine()
+    {
+        while (state == State.Roaming)
+        {
+            Vector2 roamPosition = GetRoamingPosition();
+            enemyPathfinding.MoveTo(roamPosition);
+
+
+            yield return new WaitForSeconds(2f);
+        }
+    }
+    private void Flip()
+    {
+        Vector2 currentPosition = transform.position;
+        if (currentPosition.x < lastPosition.x)
+        {
+            mySpriteRenderer.flipX = true;
+        }
+        else if (currentPosition.x > lastPosition.x)
+        {
+            mySpriteRenderer.flipX = false;
+        }
+        lastPosition = currentPosition;
+    }
+
+    private Vector2 GetRoamingPosition()
+    {
+        return new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f)).normalized;
+    }
+}
---
+++ Assets/MyGame/Scripts/Enemy/EnemyPathFinding.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[AddComponentMenu("ThinhLe/EnemyPathFinding")]
+
+public class EnemyPathFinding : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 2f;
+
+    private Rigidbody2D rb;
+    private Vector2 moveDirection;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + moveDirection * (moveSpeed * Time.fixedDeltaTime));
+    }
+
+    public void MoveTo(Vector2 targetPosition)
+    {
+        moveDirection = targetPosition;
+    }
+}
