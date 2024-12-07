45988acbbc64de05bfd028a1dfbb16be0ba63221
thinhnee <lht1823k@gmail.com>
2024-12-08 01:33:51 +0700

    update
    equip light

--- Assets/MyGame/Scripts/Others/Destructible.cs
+++ Assets/MyGame/Scripts/Others/Destructible.cs
@@ -1,16 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class Destructible : MonoBehaviour
 {
     [SerializeField] private GameObject destroyVFX;
+    [SerializeField] private GameObject playerOJ;
     private int collisionCount = 0; 
     private const int requiredCollisions = 3; 
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         PlayerHealth player = collision.gameObject.GetComponent<PlayerHealth>();
-
+        GameObject playerOJ1 = playerOJ;
+        Light2D playerLight = playerOJ1.gameObject.GetComponent<Light2D>();
+        
         if (collision.gameObject.GetComponent<AttackDamage>() || collision.gameObject.GetComponent<Projectile>() && player)
         {
             if (gameObject.CompareTag("Temp"))
@@ -21,8 +26,8 @@ public class Destructible : MonoBehaviour
                     GetComponent<ItemSpawner>().DropItem();
                     Instantiate(destroyVFX, transform.position, Quaternion.identity);
                     Destroy(gameObject);
+                    playerLight.enabled = true;
                 }
-
             }
             else
             {
